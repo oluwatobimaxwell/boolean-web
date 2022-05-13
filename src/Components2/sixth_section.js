78376238ef0f12a31421c1f3a23e38d3b82c30f8
unknown <oladosumoses24@gmail.com>
2022-05-13 17:45:59 +0100

    partners logo

--- src/Components2/sixth_section.js
+++ src/Components2/sixth_section.js
@@ -77,8 +77,8 @@ class SixthSection extends Component {
               </div>
             </div>
             {/* end client logo item */}
-                        {/* start client logo item */}
-                        <div
+            {/* start client logo item */}
+            <div
               className="col-10 col-md-3 col-sm-2 border-right border-color-medium-gray text-center sm-border-bottom xs-no-border-right wow animate__fadeIn"
               data-wow-delay="0.8s"
             >
@@ -88,7 +88,20 @@ class SixthSection extends Component {
                 </a>
               </div>
             </div>
-            {/* end client logo item */}          </div>
+            {/* end client logo item */}          
+            {/* start client logo item */}
+            <div
+              className="col-10 col-md-3 col-sm-2 border-right border-color-medium-gray text-center sm-border-bottom xs-no-border-right wow animate__fadeIn"
+              data-wow-delay="0.8s"
+            >
+              <div className="client-box padding-4-rem-tb lg-padding-3-rem-tb xs-padding-4-rem-tb">
+                <a href="#">
+                  <img src="new_images/ngd.jpg" alt="" />
+                </a>
+              </div>
+            </div>
+            {/* end client logo item */}                      
+            </div>
         </div>
       </section>
     );
