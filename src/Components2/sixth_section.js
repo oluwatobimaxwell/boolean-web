4c9a53450f9d7bab2709f5de60e5a79a2c361801
unknown <oladosumoses24@gmail.com>
2022-05-13 17:38:46 +0100

    partners logo

--- src/Components2/sixth_section.js
+++ src/Components2/sixth_section.js
@@ -12,7 +12,31 @@ class SixthSection extends Component {
             >
               <div className="client-box padding-4-rem-tb lg-padding-3-rem-tb xs-padding-4-rem-tb">
                 <a href="#">
-                  <img src="images/company-logo-24.png" alt="" />
+                  <img src="new_images/partner_3.jpg" alt="" />
+                </a>
+              </div>
+            </div>
+            {/* end client logo item */}            
+            {/* start client logo item */}
+            <div
+              className="col-10 col-md-3 col-sm-2 border-right border-bottom border-color-medium-gray text-center xs-no-border-right wow animate__fadeIn"
+              data-wow-delay="0.1s"
+            >
+              <div className="client-box padding-4-rem-tb lg-padding-3-rem-tb xs-padding-4-rem-tb">
+                <a href="#">
+                  <img src="new_images/ncc_bw.png" alt="" />
+                </a>
+              </div>
+            </div>
+            {/* end client logo item */}
+            {/* start client logo item */}
+            <div
+              className="col-10 col-md-3 col-sm-2 border-right border-bottom border-color-medium-gray text-center xs-no-border-right wow animate__fadeIn"
+              data-wow-delay="0.1s"
+            >
+              <div className="client-box padding-4-rem-tb lg-padding-3-rem-tb xs-padding-4-rem-tb">
+                <a href="#">
+                  <img src="new_images/stGregory_bw.png" alt="" />
                 </a>
               </div>
             </div>
@@ -24,7 +48,7 @@ class SixthSection extends Component {
             >
               <div className="client-box padding-4-rem-tb lg-padding-3-rem-tb xs-padding-4-rem-tb">
                 <a href="#">
-                  <img src="images/company-logo-25.png" alt="" />
+                  <img src="new_images/oya.png" alt="" />
                 </a>
               </div>
             </div>
@@ -36,19 +60,7 @@ class SixthSection extends Component {
             >
               <div className="client-box padding-4-rem-tb lg-padding-3-rem-tb xs-padding-4-rem-tb">
                 <a href="#">
-                  <img src="images/company-logo-26.png" alt="" />
-                </a>
-              </div>
-            </div>
-            {/* end client logo item */}
-            {/* start client logo item */}
-            <div
-              className="col-10 col-md-3 col-sm-2 border-bottom border-color-medium-gray text-center wow animate__fadeIn"
-              data-wow-delay="0.4s"
-            >
-              <div className="client-box padding-4-rem-tb lg-padding-3-rem-tb xs-padding-4-rem-tb">
-                <a href="#">
-                  <img src="images/company-logo-27.png" alt="" />
+                  <img src="new_images/evoke_partner.png" alt="" />
                 </a>
               </div>
             </div>
@@ -60,48 +72,23 @@ class SixthSection extends Component {
             >
               <div className="client-box padding-4-rem-tb lg-padding-3-rem-tb xs-padding-4-rem-tb">
                 <a href="#">
-                  <img src="images/company-logo-28.png" alt="" />
+                  <img src="new_images/hot_stop-bw.jpg" alt="" />
                 </a>
               </div>
             </div>
             {/* end client logo item */}
-            {/* start client logo item */}
-            <div
-              className="col-10 col-md-3 col-sm-2 border-right border-color-medium-gray text-center sm-no-border-right sm-border-bottom wow animate__fadeIn"
-              data-wow-delay="0.7s"
+                        {/* start client logo item */}
+                        <div
+              className="col-10 col-md-3 col-sm-2 border-right border-color-medium-gray text-center sm-border-bottom xs-no-border-right wow animate__fadeIn"
+              data-wow-delay="0.8s"
             >
               <div className="client-box padding-4-rem-tb lg-padding-3-rem-tb xs-padding-4-rem-tb">
                 <a href="#">
-                  <img src="images/company-logo-29.png" alt="" />
+                  <img src="new_images/last_price.jpg" alt="" />
                 </a>
               </div>
             </div>
-            {/* end client logo item */}
-            {/* start client logo item */}
-            <div
-              className="col-10 col-md-3 col-sm-2 border-right border-color-medium-gray text-center xs-no-border-right xs-border-bottom wow animate__fadeIn"
-              data-wow-delay="0.6s"
-            >
-              <div className="client-box padding-4-rem-tb lg-padding-3-rem-tb xs-padding-4-rem-tb">
-                <a href="#">
-                  <img src="images/company-logo-30.png" alt="" />
-                </a>
-              </div>
-            </div>
-            {/* end client logo item */}
-            {/* start client logo item */}
-            <div
-              className="col-10 col-md-3 col-sm-2 border-color-medium-gray text-center wow animate__fadeIn"
-              data-wow-delay="0.5s"
-            >
-              <div className="client-box padding-4-rem-tb lg-padding-3-rem-tb xs-padding-4-rem-tb">
-                <a href="#">
-                  <img src="images/company-logo-31.png" alt="" />
-                </a>
-              </div>
-            </div>
-            {/* end client logo item */}
-          </div>
+            {/* end client logo item */}          </div>
         </div>
       </section>
     );
