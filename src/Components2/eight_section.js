d21210ff3bee67341c4bce1062c7827b00fa75f0
unknown <oladosumoses24@gmail.com>
2022-05-13 11:57:10 +0100

    adding laptop mockup

--- src/Components2/eight_section.js
+++ src/Components2/eight_section.js
@@ -13,33 +13,14 @@ class EightSection extends Component {
         }}
       >
         <div className="container"
-        style={{position: "absolute", inset:0}}
         >
-          <div className="row align-items-center">
             <div
-              className="col-12 col-lg-3 md-margin-4-rem-bottom xs-margin-3-rem-bottom wow animate__fadeInLeft"
-              data-wow-delay="0.6s"
-            >
-              <div className="outside-box-left text-center text-lg-end">
-                <span className="alt-font font-weight-600 text-big text-sunglow letter-spacing-minus-10px d-block padding-3-rem-right md-no-padding-right sm-letter-spacing-minus-1-half">
-                  {/* agency */}
-                </span>
-              </div>
-            </div>
-            <div
-              className="col-12 col-lg-6 text-center text-lg-start md-margin-3-rem-bottom wow animate__fadeIn"
-              data-wow-delay="0.2s"
-            >
-              <h4 className="alt-font font-weight-500 text-extra-dark-gray mb-0 letter-spacing-minus-1px d-inline-block md-w-80 xs-w-100">
-                {/* Express your creativity and start building your design */}
-              </h4>
-            </div>
-            <div
-              className="col-12 col-lg-3 text-center wow animate__fadeIn"
+              className="wow animate__fadeIn"
               data-wow-delay="0.4s"
               style = {{
                 position: "absolute",
                 top: "0",
+                left: "5px"
               }}              
             >
               <a
@@ -49,7 +30,6 @@ class EightSection extends Component {
                 Start a project
               </a>
             </div>
-          </div>
         </div>
       </section>
     );
