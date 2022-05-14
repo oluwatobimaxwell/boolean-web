3040ae853ba266d939e68a19fe069931d99d286a
unknown <oladosumoses24@gmail.com>
2022-05-14 19:06:25 +0100

    fixed partners background

--- src/Components2/eight_section.js
+++ src/Components2/eight_section.js
@@ -3,6 +3,19 @@ class EightSection extends Component {
   render() {
     return (
       <div>
+        <div>
+            <div
+              className="wow animate__fadeIn"
+              data-wow-delay="0.4s"
+            >
+              <a
+                href="/contact-us"
+                className="btn btn-fancy btn-box-shadow btn-small btn-dark-gray"
+              >
+                Start a project
+              </a>
+            </div>
+        </div>        
         <img src="new_images/updated_boo_bg1.jpg"></img>
       </div>
     );
