b424633b8e477b0ec03bb31482b490d1bc159bed
unknown <oladosumoses24@gmail.com>
2022-05-30 09:51:21 +0100

    added Mr Ugo picture

--- src/Components/App/Team.js
+++ src/Components/App/Team.js
@@ -64,7 +64,7 @@ class Team extends React.Component {
     render() { 
         const menus = [
             {quote:"Chief Executive Officer", name: "ERIK OBIANOZIE", position:"Chief Executive Officer", picture:"images/CEO-Erik.png"},
-            {quote:"Chief Operation Officer", name: "RONALD EMERUWA", position:"Chief Operation Officer", picture:"images/blank-profile.jpg"},
+            {quote:"Chief Operation Officer", name: "RONALD EMERUWA", position:"Chief Operation Officer", picture:"images/COO_Ronald.jpg"},
             {quote:"Chief Technology Officer", name: "SHOLANKE OLUWATOBI", position:"Chief Technology Officer", picture:"images/CTO-Tobi.jpg"}
             
 
