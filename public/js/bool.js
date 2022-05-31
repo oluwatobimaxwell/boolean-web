e594530966dc2213070f1d0fd6493e0ae9c52e32
unknown <oladosumoses24@gmail.com>
2022-05-31 12:34:50 +0100

    underline active nav links

--- public/js/bool.js
+++ public/js/bool.js
@@ -1,24 +1,12 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
-    function allLink(){        
-        console.log('WFDA', $(".nav-link"))
-        for (let el of $(".nav-link")){
-            console.log(el);
-        }
-    }
-    // $(".nav-link").click(function(a){
-    //     console.log('clicked!', a)
-    //     $(this).addClass('active')
-    // })
     var url = window.location.href;
 
     // passes on every "a" tag
-    $(".nav-link").each(function() {
+    $(".nav-item a").each(function() {
         // checks if its the same on the address bar
         if (url == (this.href)) {
-            $(this).closest("li").addClass("active");
-            $(this).closest("li").addClass("underline");
+            $(this).closest("li a.nav-link").addClass("active");
+            $(this).closest("li a.nav-link").addClass("underline");
         }
     });    
-    console.log(allLink(), 'ayoooooo', $(".nav-link"))    
 });
